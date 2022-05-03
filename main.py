f9dcf4e2a3dbbaf2385716c91c431649b77ea595
José Castañeda <josedavidlg12@gmail.com>
2022-05-03 09:39:29 -0600

    main_py
    Commit python Jose C

---
+++ main.py
@@ -0,0 +1,7 @@
+#Datos de estudiante
+print("Soy estudiante de la Universidad Rafael Landivar")
+print("José David Castañeda")
+print("Primer año ingenieria en informatica y sistemas")
+print("17 años")
+print("Numero telefonico 4202 4421")
+print("De San Marcos")
